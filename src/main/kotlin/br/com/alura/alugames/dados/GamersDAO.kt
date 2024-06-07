236ea06bb89741bf35ea52e649b307b731b4f69b
fatekkl <mathtml.1105@gmail.com>
2024-06-07 11:58:46 -0300

    feat: implement generics & inheritance

--- src/main/kotlin/br/com/alura/alugames/dados/GamersDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/GamersDAO.kt
@@ -1,34 +1,29 @@
 package br.com.alura.alugames.dados
 
 import br.com.alura.alugames.modelo.Gamer
+import br.com.alura.alugames.modelo.Jogo
 import javax.persistence.EntityManager
 
-class GamersDAO(private val manager: EntityManager) {
-
-    fun getGamers(): List<Gamer> {
+class GamersDAO(manager: EntityManager): DAO<Gamer, GamerEntity>(manager = manager, GamerEntity::class.java) {
 
 
-        val query = manager.createQuery("FROM GamerEntity", GamerEntity::class.java)
-
-        return query.resultList.map { entity ->
-            Gamer(
-                entity.nome,
-                entity.email,
-                entity.dataNascimento,
-                entity.usuario,
-                entity.id
-            )
-        }
+    override fun toModel(entity: GamerEntity): Gamer {
+        return Gamer(
+            entity.nome,
+            entity.email,
+            entity.dataNascimento,
+            entity.usuario,
+            entity.id
+        )
     }
 
-    fun adicionarGamer(gamer: Gamer) {
-        val entity = GamerEntity(gamer.nome, gamer.email, gamer.dataNascimento, gamer.usuario)
-
-        manager.transaction.begin()
-        manager.persist(entity)
-        manager.transaction.commit()
-
+    override fun toEntity(objeto: Gamer): GamerEntity {
+        return GamerEntity(
+            objeto.nome,
+            objeto.email,
+            objeto.dataNascimento,
+            objeto.usuario,
+            objeto.id
+        )
     }
-
-
 }
